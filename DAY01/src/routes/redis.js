42caf2606a9e93a79097db4745a4ff3588e370aa
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2026-02-06 21:48:39 +0530

    create middleware

---
+++ DAY01/src/routes/redis.js
@@ -0,0 +1,12 @@
+const { createClient }  = require('redis');
+
+const redisClient = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: 'redis-18094.c305.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 18094
+    }
+});
+
+module.exports = redisClient;
