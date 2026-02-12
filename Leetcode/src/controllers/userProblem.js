01318596cfb18c34dacd0321c58db1438c5a39cb
ayushkatiyar31 <ayushkatiyar827@gmail.com>
2026-02-12 14:12:55 +0530

    update userProblem

--- Leetcode/src/controllers/userProblem.js
+++ Leetcode/src/controllers/userProblem.js
@@ -1,5 +1,6 @@
 const {getLanguageById,submitBatch,submitToken} = require("../utils/problemUtility");
-const Problem = require("../models/problem")
+const Problem = require("../models/problem");
+const User = require("../models/user");
 
 const createProblem = async (req,res)=>{
 
@@ -195,9 +196,27 @@ const getAllProblem = async(req,res)=>{
 }
 
 
+const solvedAllProblembyUser =  async(req,res)=>{
+   
+    try{
+       
+      const userId = req.result._id;
+
+      const user =  await User.findById(userId).populate({
+        path:"problemSolved",
+        select:"_id title difficulty tags"
+      });
+      
+      res.status(200).send(user.problemSolved);
+
+    }
+    catch(err){
+      res.status(500).send("Server Error");
+    }
+}
 
 
 
-module.exports = {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem};
+module.exports = {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblembyUser};
 
 
